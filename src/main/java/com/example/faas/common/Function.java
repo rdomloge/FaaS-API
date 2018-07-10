a0ce4a247481a0efb29dc707ca6f8810c851f215
Ramsay Domloge <ramsay.domloge@bca.com>
2018-07-10 19:43:28 +0100

    Removed dep on DTO

--- src/main/java/com/example/faas/common/Function.java
+++ src/main/java/com/example/faas/common/Function.java
@@ -3,13 +3,11 @@ package com.example.faas.common;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
-import com.example.faas.dto.JobRequest;
-
 public interface Function<V> extends Callable<V>{
 	
 	V call();
 
-	void setRequest(JobRequest request);
+	void setJobParams(Map<String, String> params);
 	
-	void setConfig(Map<String, String> config);
+	void setStaticConfig(Map<String, String> config);
 }
